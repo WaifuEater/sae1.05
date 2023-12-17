01c15a047f26fea36a9fdc1f7cd27ec81bb0763d
WaifuEater <97566451+WaifuEater@users.noreply.github.com>
2023-12-17 11:14:11 +0100

    Update main.py
    ajout de -a et de ctrl + c pour arreter le programme
    On a -a -t -h pour le moment

--- sae1.05/main.py
+++ sae1.05/main.py
@@ -17,23 +17,33 @@ def signal_handler(sig, frame):
     print("\nCtrl + C pressed [·]\nExiting...")
     sys.exit(0)
 
-# Fonction pour vérifier une adresse IP
-def check_ip(ip):
+# Fonction pour vérifier une plage d'adresses IP
+def check_mult_ip(ip):
     try:
-        # Boucle pour vérifier chaque adresse de la plage
         for i in range(256):
             x = f"{ip}.{i}"
             paquet = IP(dst=x) / ICMP()
-            print(x)
+            print("scan de ", x)
             send(paquet)
             reply = sr1(paquet, timeout=3)
-            # Vérifie si la réponse est reçue ou non
             if reply is not None:
-                print(f"{x} is online")
+                print(f"{x} est ONLINE")
             else:
-                print("Timeout waiting for %s" % paquet[IP].dst)
-    except KeyboardInterrupt:
-        print("Ctrl + C pressed [·]\nExiting...")
+                print("%s n'est pas joignable pour le moment" % paquet[IP].dst)
+    except Exception as e:
+        print("Une erreur est survenue:", e)
+
+# Fonction pour vérifier une seule adresse IP
+def check_ip(ip):
+    try:
+        paquet = IP(dst=ip) / ICMP()
+        print("scan de ", ip)
+        send(paquet)
+        reply = sr1(paquet, timeout=3)
+        if reply is not None:
+            print(f"{ip} est ONLINE")
+        else:
+            print("%s n'est pas joignable pour le moment" % paquet[IP].dst)
     except Exception as e:
         print("Une erreur est survenue:", e)
 
@@ -43,6 +53,7 @@ def main():
     parser = argparse.ArgumentParser(description='Scan IP addresses.')
     parser.add_argument('ip', type=str, help='Adresse IP à vérifier')
     parser.add_argument('-t', action='store_true', help='Exécute la vérification pour toutes les adresses IP')
+    parser.add_argument('-a', action='store_true', help='Executer la vérification pour une seule adresse IP')
 
     # Analyse des arguments de la ligne de commande
     args = parser.parse_args()
@@ -51,11 +62,13 @@ def main():
     # Gestion du signal d'interruption (Ctrl + C)
     signal.signal(signal.SIGINT, signal_handler)
 
-    # Vérification si l'option -t est utilisée pour exécuter la boucle for
+    # Choix du mode de vérification en fonction des arguments
     if args.t:
-        check_ip(ip_to_check)  # Appel de la fonction pour vérifier toutes les adresses
+        check_mult_ip(ip_to_check)
+    elif args.a:
+        check_ip(ip_to_check)
     else:
-        print(f"Adresse IP à vérifier : {ip_to_check}")  # Affichage de l'adresse IP spécifiée
+        print(f"Adresse IP à vérifier : {ip_to_check}")
 
 # Point d'entrée du script
 if __name__ == "__main__":
