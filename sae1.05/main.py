213b0fe768254dbde18f3df1b921019a53b54a78
WaifuEater <97566451+WaifuEater@users.noreply.github.com>
2023-12-17 10:44:42 +0100

    Update main.py
    remplacement du -a par -t car mauvaise option

--- sae1.05/main.py
+++ sae1.05/main.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import argparse
 
 # common = [80, 443, 67, 68, 20, 21, 23, 22, 53, 8080, 123, 25, 3389, 110, 554, 445, 587, 993, 137, 139, 8008, 500, 143, 161, 162, 389, 1434, 5900] 
 # liste des 28 ports les plus utilisés
@@ -10,19 +11,14 @@ for i in range(len(common)):
     # envoi une trame a tout les ports de la liste common 
    """
 
-import argparse
-
-# Fonction pour vérifier une adresse IP
 def check_ip(ip):
     try:
-        # Boucle pour vérifier chaque adresse de la plage
         for i in range(256):
             x = f"{ip}.{i}"
-            paquet = IP(dst=x) / ICMP() # Ajoute l'ip et le protocole icmp au paquet
+            paquet = IP(dst=x) / ICMP()
             print(x)
             send(paquet)
             reply = sr1(paquet, timeout=3)
-            # Vérifie si la réponse est reçue ou non
             if reply is not None:
                 print(f"{x} is online")
             else:
@@ -32,24 +28,19 @@ def check_ip(ip):
     except Exception as e:
         print("Une erreur est survenue:", e)
 
-# Fonction principale
 def main():
-    # Définition des arguments en utilisant argparse
     parser = argparse.ArgumentParser(description='Scan IP addresses.')
     parser.add_argument('ip', type=str, help='Adresse IP à vérifier')
-    parser.add_argument('-a', action='store_true', help='Exécute la vérification pour toutes les adresses IP')
+    parser.add_argument('-t', action='store_true', help='Exécute la vérification pour toutes les adresses IP')
 
-    # Analyse des arguments de la ligne de commande
     args = parser.parse_args()
     ip_to_check = args.ip
 
-    # Vérification si l'option -a est utilisée pour exécuter la boucle for
-    if args.a:
-        check_ip(ip_to_check)  # Appel de la fonction pour vérifier toutes les adresses
+    if args.t:
+        check_ip(ip_to_check)
     else:
-        print(f"Adresse IP à vérifier : {ip_to_check}")  # Affichage de l'adresse IP spécifiée
+        print(f"Adresse IP à vérifier : {ip_to_check}")
 
-# Point d'entrée du script
 if __name__ == "__main__":
     main()
 
