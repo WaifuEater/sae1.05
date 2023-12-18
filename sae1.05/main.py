be3aba069b3f4654328b7279413e712db6774a54
WaifuEater <97566451+WaifuEater@users.noreply.github.com>
2023-12-18 17:45:25 +0100

    ajout de -p

--- sae1.05/main.py
+++ sae1.05/main.py
@@ -1,8 +1,11 @@
 from scapy.all import *
 import argparse
 import signal
+import ipaddress
+import time
 
-# common = [80, 443, 67, 68, 20, 21, 23, 22, 53, 8080, 123, 25, 3389, 110, 554, 445, 587, 993, 137, 139, 8008, 500, 143, 161, 162, 389, 1434, 5900] 
+
+common = [80, 443, 67, 68, 20, 21, 23, 22, 53, 8080, 123, 25, 3389, 110, 554, 445, 587, 993, 137, 139, 8008, 500, 143, 161, 162, 389, 1434, 5900] 
 # liste des 28 ports les plus utilisés
 """p=input("ip : ")
 for i in range(len(common)):
@@ -11,27 +14,36 @@ for i in range(len(common)):
     send(paquet)
     # envoi une trame a tout les ports de la liste common 
    """
-
+ip_valides = list()
 # Fonction pour gérer le signal d'interruption (Ctrl + C)
 def signal_handler(sig, frame):
     print("\nCtrl + C pressed [·]\nExiting...")
     sys.exit(0)
-
+classe=0
 # Fonction pour vérifier une plage d'adresses IP
 def check_mult_ip(ip):
     try:
-        for i in range(256):
+        if "/" in ip:
+            ip, subnet = ip.split("/")
+            octets = ip.split(".")
+            subnet = int(subnet)
+            binary_ip = ''.join([format(int(octet), '08b') for octet in octets])
+            network_address = '.'.join([str(int(binary_ip[i:i+subnet], 2)) for i in range(0, 32, subnet)])
+            print("Adresse réseau: ", "ip")
+        # Reste du code pour la vérification des adress
+        for i in range(3):
             x = f"{ip}.{i}"
             paquet = IP(dst=x) / ICMP()
             print("scan de ", x)
             send(paquet)
             reply = sr1(paquet, timeout=3)
             if reply is not None:
+                ip_valides.append(i)
                 print(f"[✓] {x} est ONLINE [✓]")
             else:
                 print("[X] %s n'est pas joignable pour le moment [X]" % paquet[IP].dst)
     except Exception as e:
-        print("[?] Une erreur est survenue: [?]", e)
+        print("[?] Une erreur est survenue: [?] \n Vérifiez que le format de ip est x.x.x.x/classe ou x.x.x \n", e)
 
 # Fonction pour vérifier une seule adresse IP
 def check_ip(ip):
@@ -47,13 +59,72 @@ def check_ip(ip):
     except Exception as e:
         print("[?] Une erreur est survenue: [?]", e)
 
+
+def port_check(ip):
+    ports_valides = list()
+    try:
+        for i in common :
+            x = common
+            packet = IP(dst=ip) / TCP(dport=x, flags="S")
+            send(packet)
+            print("scan du port ", i)
+            reply = sr1(packet, timeout=3)
+            if reply is not None:
+                ports_valides.append(i)
+                print(f"[✓] port {i} est ONLINE [✓]")
+            else:
+                print("[X] Le port %s n'est pas joignable pour le moment [X]" % i)
+    except Exception as e:
+        print("[?] Une erreur est survenue: [?] \n ", e)
+
+
+def arp_check(ip):
+    ip_found = False  # Indique si l'IP a été trouvée
+
+    def arp_display(pkt):
+        nonlocal ip_found
+        if ARP in pkt and pkt[ARP].op == 2 and pkt[ARP].psrc == ip:
+            print(f"ARP from: {pkt[ARP].psrc} to {pkt[ARP].pdst}")
+            print(f"{ip} est bien présente dans le trafic ARP.")
+            mac_address = pkt[ARP].hwsrc  # Récupère l'adresse MAC
+            print(f"Adresse MAC de {ip}: {mac_address}")
+            ip_found = True
+            sys.exit(0)
+
+    try:
+        print("Écoute du trafic ARP...")
+        duration = int(input("Entrez la durée d'écoute en secondes : "))
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            # Envoie une requête ARP spécifique pour obtenir la réponse contenant l'adresse MAC
+            ans, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip), timeout=2, verbose=False)
+            for _, pkt in ans:
+                arp_display(pkt)
+            time.sleep(1)  # Attente avant de réessayer la requête ARP
+
+        # Si l'IP n'est pas trouvée pendant la durée spécifiée
+        if not ip_found:
+            print(f"Aucune activité ARP pour l'adresse IP {ip} détectée pendant {duration} secondes.")
+    except Exception as e:
+        print("[?] Une erreur est survenue lors de l'écoute ARP: [?]", e)
+
+
+    
+    
+    
+
 # Fonction principale
 def main():
     # Définition des arguments en utilisant argparse
     parser = argparse.ArgumentParser(description='Scan IP addresses.')
     parser.add_argument('ip', type=str, help='Adresse IP à vérifier')
     parser.add_argument('-t', action='store_true', help='Exécute la vérification pour toutes les adresses IP')
-    parser.add_argument('-a', action='store_true', help='Executer la vérification pour une seule adresse IP')
+    parser.add_argument('-a', action='store_true', help='Execute la vérification pour une seule adresse IP')
+    parser.add_argument('-l', action='store_true', help='Liste les addresse ip ayant répondu')
+    parser.add_argument('-po', action='store_true', help='Execute la vérification des 25 ports les plus communs')
+    parser.add_argument('-p', action='store_true', help=f"Ecoute le traffic ARP pour trouver l'hote") 
+ 
+
 
     # Analyse des arguments de la ligne de commande
     args = parser.parse_args()
@@ -65,10 +136,18 @@ def main():
     # Choix du mode de vérification en fonction des arguments
     if args.t:
         check_mult_ip(ip_to_check)
+    elif args.p:
+        arp_check(ip_to_check)
     elif args.a:
         check_ip(ip_to_check)
+    elif args.l:
+        print(ip_valides)
+    if args.po:
+        port_check(ip_to_check)
     else:
         print(f"Adresse IP à vérifier : {ip_to_check}")
+    signal.signal(signal.SIGINT, signal_handler)
+    
 
 # Point d'entrée du script
 if __name__ == "__main__":
@@ -85,3 +164,4 @@ if __name__ == "__main__":
 # for element in non_rep : # element représente un couple (paquet émis, paquet reçu)
 # 	if element[1][ICMP].type == 8 : # 8 <=> echo-request voir page de Wikipedia
 # 		print( element[0][IP].dst + ' : aucun echo-reply ')
+                      
